5ef46b61cff5b2f379364b7555ea83d926e84b93
Yixuan Xu <yixuanxu94@outlook.com>
2023-03-22 03:08:12 +0800

    adjust font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
-import { Container } from '~/components/container';
+import { Noto_Serif_SC } from 'next/font/google'
+
+const Noto = Noto_Serif_SC({ 
+  subsets: ['latin'], 
+  weight: ['400', '500', '700'],
+})
+
 export const metadata = {
   title: '许一旋-软件工程师-简历',
   description: '许一旋 软件工程师 南安普顿大学-硕士',
@@ -13,13 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="zh-CN">
-      <head>
-        <link rel="icon" type="image/svg+xml" href="./favicon.svg"/>
-      </head>
       <body>
-        <Container>
+        <main className={`p-5 mx-auto md:p-0 md:w-1/2 md:my-10 ${Noto.className}`}>
           {children}
-        </Container>
+        </main>
         <Analytics />
       </body>
     </html>
--- fonts/noto.ts
+++
@@ -1,7 +0,0 @@
-import { Noto_Serif_SC } from 'next/font/google'
-
-const Noto = Noto_Serif_SC({ subsets: ['latin'], weight: ['400', '500', '700'] })
-
-export {
-  Noto
-}
