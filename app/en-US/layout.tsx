622c01de5e181c86b72337018b5cf7e2b42ca650
Yixuan Xu <yixuanxu94@outlook.com>
2023-07-13 10:46:07 +0800

    use serif font

--- app/en-US/layout.tsx
+++ app/en-US/layout.tsx
@@ -1,6 +1,6 @@
-import { Inter as Inter_Font } from 'next/font/google'
+import { Roboto_Serif } from 'next/font/google'
 
-const Inter = Inter_Font({
+const Roboto = Roboto_Serif({
   subsets: ['latin'],
   weight: ['400', '500', '700'],
   variable: '--font-serif-sc'
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <main className={`p-5 mx-auto md:p-0 md:w-1/2 md:my-10 ${Inter.variable} font-sans`}>
+    <main className={`p-5 mx-auto md:p-0 md:w-1/2 md:my-10 ${Roboto.variable} font-sans`}>
       {children}
     </main>
   )
